49d10028c706969aaada620d4d4beb23f50ddcec
oferns <oferns@gmail.com>
2016-04-05 15:51:35 +0100

    Login page, style only

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,18 @@ module.exports = function() {
     router.route('/')
         .get(function(req, res, next) {
             return res.render('home');
-        })
+        });
         
 
     router.route('/style')
         .get(function(req, res, next) {
             return res.render('style');
-        })
-       
+        });
+        
+    router.route('/login')
+        .get(function(req, res, next){
+            return res.render('login', {'errors' : {}, 'login':{} });            
+        });
+        
     return router;
 }
