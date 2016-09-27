770e28b1951d1c44a6adac24d01d6ab2a72bd284
Ollie Ferns <oferns@gmail.com>
2016-09-27 10:14:26 +0100

    Minor changes

--- scripts/modules/ajax.js
+++ scripts/modules/ajax.js
@@ -4,29 +4,55 @@
 
 define(function () {
 
-    var init = function(){
-        var row = document.getElementsByClassName('row')[0];               
-        history.replaceState(row.outerHTML,'',document.location.href);
+    var init = function () {
+        var row = document.getElementsByClassName('row')[0];
+        history.replaceState(row.outerHTML, '', document.location.href);
     }
-    window.onpopstate = function(event){
-        var row = document.getElementsByClassName('row')[0];       
+    window.onpopstate = function (event) {
+        var row = document.getElementsByClassName('row')[0];
         var oldrow = document.createRange().createContextualFragment(event.state);
         var parent = row.parentNode;
         parent.replaceChild(oldrow, row);
     };
 
-    var findParentRow = function(element){
+
+    function fadeout(element) {
+        var op = 1;  // initial opacity
+        var timer = setInterval(function () {
+            if (op <= 0.1) {
+                clearInterval(timer);
+                element.style.display = 'none';
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op -= op * 0.1;
+        }, 50);
+    };
+
+    function fadein(element) {
+        var op = 0.1;  // initial opacity
+        element.style.display = 'block';
+        var timer = setInterval(function () {
+            if (op >= 1) {
+                clearInterval(timer);
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op += op * 0.1;
+        }, 10);
+    }
+    var findParentRow = function (element) {
         var parent = element.parentNode;
-        while(parent){
-            if(parent.classList && parent.classList.contains('row')){
+        while (parent) {
+            if (parent.classList && parent.classList.contains('row')) {
                 return parent;
             };
             parent = parent.parentNode;
         }
 
         var parents = element.ownerDocument.getElementsByClassName('row');
-        
-        if(parents.length){
+
+        if (parents.length) {
             return parents[0];
         }
 
@@ -48,13 +74,13 @@ define(function () {
         xhr.open('GET', anchor.href);
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.onload = function () {
-            if(xhr.status >= 200 && xhr.status < 400){
-                var row  = findParentRow(anchor);
+            if (xhr.status >= 200 && xhr.status < 400) {
+                var row = findParentRow(anchor);
                 var parent = row.parentNode;
                 var newrow = document.createRange().createContextualFragment(xhr.responseText);
                 parent.replaceChild(newrow, row);
                 history.pushState(xhr.responseText, '', anchor.href);
-            } else{
+            } else {
                 alert('failed! ' + xhr.status);
             };
         }
@@ -66,9 +92,9 @@ define(function () {
         element.addEventListener('click', function (e) {
             switch (e.target.tagName) {
                 case 'A':
-                e.preventDefault(); 
-                ajaxPage(e.target);
-                break;
+                    e.preventDefault();
+                    ajaxPage(e.target);
+                    break;
                 default: break;
             };
         });
